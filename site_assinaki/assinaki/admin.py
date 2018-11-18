2eac6b439b8b232580a9aa31503905b980495be4
tmenegaz <tmenegaz77@gmail.com>
2018-11-18 17:21:17 -0300

    preparação  do modelo

--- site_assinaki/assinaki/admin.py
+++ site_assinaki/assinaki/admin.py
@@ -1,3 +1,13 @@
+from pyexpat import model
+
 from django.contrib import admin
 
 # Register your models here.
+from .models import Cliente
+
+class AdminCliente(admin.ModelAdmin):
+    list_display = ["nome", "sobrenome", "data_nascimento", "cpf", "cnpj", "telefone_fixo", "telefone_celular", "e_mail", "timestamp"]
+    class Meta:
+        model = Cliente
+
+admin.site.register(Cliente, AdminCliente)
--- site_assinaki/assinaki/models.py
+++ site_assinaki/assinaki/models.py
@@ -2,15 +2,19 @@ from django.db import models
 from django.core.validators import EmailValidator
 
 # Create your models here.
-class Clientes(models.Model):
+class Cliente(models.Model):
     nome = models.CharField(max_length=150)
     sobrenome = models.CharField(max_length=150)
-    data_nascimento = models.DateTimeField(auto_now=False, auto_now_add=False)
+    data_nascimento = models.DateField(auto_now=False, auto_now_add=False)
     cpf = models.CharField(max_length=11)
     cnpj = models.CharField(max_length=14)
     telefone_fixo = models.CharField(max_length=15)
     telefone_celular = models.CharField(max_length=15)
     e_mail = models.EmailField(validators=[EmailValidator])
+    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+
+    def __str__(self):
+        return self.e_mail
 
     # tabelas separadas
     # pais =
