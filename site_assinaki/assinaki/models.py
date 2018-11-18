49ff201b0151e763be38159e8403da4388571aee
tmenegaz <tmenegaz77@gmail.com>
2018-11-18 15:59:41 -0300

    preparação  do modelo

--- site_assinaki/assinaki/models.py
+++ site_assinaki/assinaki/models.py
@@ -1,6 +1,26 @@
 from django.db import models
+from django.core.validators import EmailValidator
 
 # Create your models here.
 class Clientes(models.Model):
-    nome =
-    sobrenome =
+    nome = models.CharField(max_length=150)
+    sobrenome = models.CharField(max_length=150)
+    data_nascimento = models.DateTimeField(auto_now=False, auto_now_add=False)
+    cpf = models.CharField(max_length=11)
+    cnpj = models.CharField(max_length=14)
+    telefone_fixo = models.CharField(max_length=15)
+    telefone_celular = models.CharField(max_length=15)
+    e_mail = models.EmailField(validators=[EmailValidator])
+
+    # tabelas separadas
+    # pais =
+    # estado =
+    # cidade =
+    # rua =
+    # número =
+    # complemento =
+
+    # tabela de login
+    # login =
+    # senha =
+    # confirma_senha =
